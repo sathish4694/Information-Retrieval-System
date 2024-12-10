12f5fd48bfa598dec7db612ec038f992f6bbb5e1
Your Name <you@example.com>
2024-12-10 16:49:08 -0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Information Retrieval Syastem',
+    version= '0.0.0',
+    author= 'Sathish',
+    author_email= 'sathish31294@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
